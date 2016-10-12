8d76a6228200f77da399b78ff7ea50dc84082335
Adam Labadorf <labadorf@bu.edu>
2016-10-12 17:24:49 -0400

    boAdd boRound of boText boStat boOutput

--- scripts/boStatistics.js
+++ scripts/boStatistics.js
@@ -8,17 +8,17 @@ define(function(require) {
   }
 
   function _update(stat, delta) {
-    $("#boStat_boPoints").text(bobo.state('boPoints'));
-    $("#boStat_boCharm").text(bobo.state('boCharm'));
-    $("#boStat_boConquests").text(bobo.state('boConquests'));
+    $("#boStat_boPoints").text(Math.round(bobo.state('boPoints')));
+    $("#boStat_boCharm").text(Math.round(bobo.state('boCharm')));
+    $("#boStat_boConquests").text(Math.round(bobo.state('boConquests')));
 
-    $("#boStat_boMoves").text(bobo.state('boMoves'));
-    $("#boStat_boKnowledge").text(bobo.state('boKnowledge'));
-    $("#boStat_boAcademicCredits").text(bobo.state('boAcademicCredits'));
+    $("#boStat_boMoves").text(Math.round(bobo.state('boMoves')));
+    $("#boStat_boKnowledge").text(Math.round(bobo.state('boKnowledge')));
+    $("#boStat_boAcademicCredits").text(Math.round(bobo.state('boAcademicCredits')));
 
-    $("#boStat_boBucks").text(bobo.state('boBucks'));
-    $("#boStat_boStrength").text(bobo.state('boStrength'));
-    $("#boStat_boDrunkitude").text(bobo.state('boDrunkitude'));
+    $("#boStat_boBucks").text(Math.round(bobo.state('boBucks')));
+    $("#boStat_boStrength").text(Math.round(bobo.state('boStrength')));
+    $("#boStat_boDrunkitude").text(Math.round(bobo.state('boDrunkitude')));
 
     if(delta > 0) {
         $("#boCell_" + stat).css('background', '#008800');
