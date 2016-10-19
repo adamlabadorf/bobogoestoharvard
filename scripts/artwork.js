4dd8378fda46f4e446806e9d8da8d4fce32dde5a
Adam Labadorf <labadorf@bu.edu>
2016-10-18 21:15:49 -0400

    Fixed non-looping sprite animation logic

--- scripts/artwork.js
+++ scripts/artwork.js
@@ -3,25 +3,44 @@ define(function(require) {
   var art = {};
 
   art['map'] = require('scripts/artwork/map.js');
+  art['gym'] = require('scripts/artwork/gym.js');
 
   createSprite = function(collection,name,loop=true) {
-    var sprite = {};
-    sprite.art = art[collection][name];
-    sprite.current_i = 0;
-    sprite.current = sprite.art[sprite.current_i];
-    sprite.step = function() {
-      // override this if you need to add more behavior to sprite on step
-    }
-    sprite.next = function() { // moves to next image in art sequence
-      if(loop) {
-        this.current_i = (this.current_i+1)%this.art.length;
-      } else {
-        this.current_i = Math.min(this.current_i+1,this.art.length);
-      }
-      this.current = this.art[this.current_i];
-      this.step();
-      return this.current;
-    }
+    var sprite = {
+        art: art[collection][name],
+        collection: collection,
+        name: name,
+        current_i: 0,
+        current: art[collection][name][0],
+        running: loop,
+        run: function(run) {
+            this.running = run;
+        },
+        step: function(f=undefined) {
+            // set the sprite step function if passed
+            if(f) {
+                this._step_f = f;
+            }
+            // call step function if it was provided at some point
+            if(this._step_f) {
+                this._step_f(this);
+            }
+        },
+        next: function() { // moves to next image in art sequence
+            if(this.running) {
+              this.current_i = Math.min(this.current_i+1,this.art.length);
+              if(this.current_i == this.art.length) {
+                this.current_i = 0;
+                if(!loop) {
+                    this.run(false);
+                }
+              }
+            }
+            this.current = this.art[this.current_i];
+            this.step();
+            return this.current;
+        }
+    };
     return sprite;
   }
 
